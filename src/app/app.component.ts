51d6c4d5238824736458b2aaf1385d8677233bc3
jmdaugherty94@gmail.com <jmdaugherty94@gmail>
2021-03-30 08:51:04 -0400

    latest add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world-angular';
+  title = 'hello-world-angular from Jonathan(future programmer)';
+  name: string = "Jonathan Daugherty";
+  myText: string = "Default myText String"
+
+  changeName(): void {
+    this.name = "Michael Jordan";
+  }
 }
